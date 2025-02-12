da1b4feed7edd89042125a4b82292064c40afc01
MaelleCoquelet <maelle.coquelet@edu.univ-fcomte.fr>
2025-02-12 23:47:05 +0100

    Ajout de la gestion des films avec création de la collection, ajout de plusieurs films et mise à jour de la mise en page pour afficher la liste des films.

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,5 @@
 import { glob } from "astro/loaders";
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const personne = defineCollection({
     loader: glob({ base: "src/data/personne", pattern: "**/*.md" }),
@@ -9,21 +9,32 @@ const personne = defineCollection({
         dateNaissance: z.date(),
         dateDeces: z.date().optional(),
         nationalite: z.string(),
-        profession: z.array(
-            z.enum([
-                "acteur",
-                "realisateur",
-                "scenariste",
-                "producteur",
-                "compositeur",
-                "monteur",
-                "directeur-photo",
-                "costumier",
-                "maquilleur",
-                "effets-speciaux",
-                "effets-visuels",
-            ])
-        ).optional(),
+        profession: z
+            .array(
+                z.enum([
+                    "acteur",
+                    "realisateur",
+                    "scenariste",
+                    "producteur",
+                    "compositeur",
+                    "monteur",
+                    "directeur-photo",
+                    "costumier",
+                    "maquilleur",
+                    "effets-speciaux",
+                    "effets-visuels",
+                ])
+            )
+            .optional(),
     }),
 });
-export const collections = { personne };
+
+const films = defineCollection({
+    loader: glob({ base: "src/data/films", pattern: "**/*.md" }),
+    schema: z.object({
+        titre: z.string(),
+        dateSortie: z.date(),
+        realisateur: reference("personne").optional(),
+    }),
+});
+export const collections = { personne, films };
